edb9c8eefc8157ee87c56631136d38dda26395b5
d51x <dimonich110@gmail.com>
2020-12-23 17:34:02 +0300

    http: подключение embed_txtfiles перенес из основного модуля
    Signed-off-by: d51x <dimonich110@gmail.com>

--- http/component.mk
+++ http/component.mk
@@ -4,3 +4,10 @@
 
 COMPONENT_SRCDIRS := . lib
 COMPONENT_ADD_INCLUDEDIRS := include
+
+EXTRA_COMPONENT_DIRS := components
+COMPONENT_EMBED_FILES := 	${PROJECT_PATH}/${EXTRA_COMPONENT_DIRS}/web/favicon.ico \
+							${PROJECT_PATH}/${EXTRA_COMPONENT_DIRS}/web/main.min.css \
+							${PROJECT_PATH}/${EXTRA_COMPONENT_DIRS}/web/ajax.min.js	\
+							${PROJECT_PATH}/${EXTRA_COMPONENT_DIRS}/web/menu.png	\
+							${PROJECT_PATH}/${EXTRA_COMPONENT_DIRS}/web/menu2.png
